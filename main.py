b44084fd94f2ac25a39758be796f5d5371151f64
vilhess <samy.vilhes@gmail.com>
2025-03-10 10:29:07 +0100

    fresh

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
 import gc
 
-from dataset.nab import get_loaders as get_nab_loaders
-from dataset.nasa import get_loaders as get_nasa_loaders, smapfiles, mslfiles
-from dataset.smd import get_loaders as get_smd_loaders, machines
-from dataset.swat import get_loaders as get_swat_loaders
-
 from eval.get_metric import get_metrics
-from utils import save_results
+from utils import load_model, get_loaders, save_results
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
@@ -36,47 +31,9 @@ def main(cfg: DictConfig):
     method = cfg.method.name
     threshold = cfg.dataset.threshold
     
-    if model_name=="patchtrad":
-        from models.patchtrad import PatchTradLit as model
-    elif model_name=="aelstm":
-        from models.aelstm import AELSTMLit as model
-    elif model_name=="tranad":
-        from models.tranad import TranADLit as model
-    elif model_name=="lstm":
-        from models.lstm import LSTMLit as model
-    elif model_name=="fedformer":
-        from models.fedformer import FEDformerLit as model
-    elif model_name=="patchad":
-        from models.patchad import PatchADLit as model
-    elif model_name=="doc":
-        from models.doc import DOCLit as model
-    elif model_name=="anotrans":
-        from models.anotrans import AnomalyTransformerLit as model
-    elif model_name=="dcdetector":
-        from models.dcdetector import DCDetectorLit as model
-    elif model_name=="drocc":
-        from models.drocc import DROCCLit as model
-    elif model_name=="patchtst":
-        from models.patchtst import PatchTSTLit as model
-    elif model_name=="usad":
-        from models.usad import USADLit as model
-    elif model_name=="madgan":
-        from models.madgan import MADGANLit as model
-    elif model_name=="diffae":
-        from models.diffae import FastDiffNetLit as model
+    model = load_model(model_name)
 
-    av_datasets = ["nyc_taxi", "smd", "smap", "msl", "swat", "ec2_request_latency_system_failure"]
-    assert dataset in av_datasets, f"Dataset ({dataset}) should be in {av_datasets}"
-
-    if dataset in ["ec2_request_latency_system_failure", "nyc_taxi"]:
-        loaders = [get_nab_loaders(window_size=config.ws, root_dir="data/nab", dataset=dataset, batch_size=config.bs)]
-    elif dataset in ["smap", "msl"]:
-        file = smapfiles if dataset == "smap" else mslfiles
-        loaders = [get_nasa_loaders(window_size=config.ws, root_dir="data/nasa", dataset=dataset, filename=f, batch_size=config.bs) for f in file]
-    elif dataset == "smd":
-        loaders = [get_smd_loaders(window_size=config.ws, root_dir="data/smd/processed", machine=m, batch_size=config.bs) for m in machines]
-    elif dataset == "swat":
-        loaders = [get_swat_loaders(window_size=config.ws, root_dir="data/swat", batch_size=config.bs)]
+    loaders = get_loaders(dataset, config)
 
     #wandb_logger = WandbLogger(project='DL4TSAD', name=f"{model_name}_{dataset}")
 
@@ -146,7 +103,7 @@ def main(cfg: DictConfig):
     #print(f"Final F1: {final_f1}")
     #print(f"Final F1-Adjusted: {final_adjusted}")
 
-    save_results(filename="results/aucs.json", dataset=dataset, model=f'{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}', score=round(final_auc, 4))
+    #save_results(filename="results/aucs.json", dataset=dataset, model=f'{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}', score=round(final_auc, 4))
     #save_results(filename="results/f1.json", dataset=dataset, model=f"{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}_{method}", score=round(final_f1, 4))
     #save_results(filename="results/f1_adjusted.json", dataset=dataset, model=f"{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}_{method}", score=round(final_adjusted, 4))
 
--- utils.py
+++ utils.py
@@ -1,5 +1,63 @@
 import json
 
+def load_model(model_name):
+    if model_name=="patchtrad":
+        from models.patchtrad import PatchTradLit as model
+    elif model_name=="aelstm":
+        from models.aelstm import AELSTMLit as model
+    elif model_name=="tranad":
+        from models.tranad import TranADLit as model
+    elif model_name=="lstm":
+        from models.lstm import LSTMLit as model
+    elif model_name=="fedformer":
+        from models.fedformer import FEDformerLit as model
+    elif model_name=="patchad":
+        from models.patchad import PatchADLit as model
+    elif model_name=="doc":
+        from models.doc import DOCLit as model
+    elif model_name=="anotrans":
+        from models.anotrans import AnomalyTransformerLit as model
+    elif model_name=="dcdetector":
+        from models.dcdetector import DCDetectorLit as model
+    elif model_name=="drocc":
+        from models.drocc import DROCCLit as model
+    elif model_name=="patchtst":
+        from models.patchtst import PatchTSTLit as model
+    elif model_name=="usad":
+        from models.usad import USADLit as model
+    elif model_name=="madgan":
+        from models.madgan import MADGANLit as model
+    elif model_name=="diffae":
+        from models.diffae import FastDiffNetLit as model
+    else:
+        assert False, f"{model_name} is not implemented"
+    return model
+
+def get_loaders(dataset, config):
+    av_datasets = ["nyc_taxi", "smd", "smap", "msl", "swat", "ec2_request_latency_system_failure"]
+    assert dataset in av_datasets, f"Dataset ({dataset}) should be in {av_datasets}"
+
+    if dataset in ["ec2_request_latency_system_failure", "nyc_taxi"]:
+        from dataset.nab import get_loaders as get_nab_loaders
+        loaders = [get_nab_loaders(window_size=config.ws, root_dir="data/nab", dataset=dataset, batch_size=config.bs)]
+
+    elif dataset in ["smap", "msl"]:
+        from dataset.nasa import get_loaders as get_nasa_loaders, smapfiles, mslfiles
+        file = smapfiles if dataset == "smap" else mslfiles
+        loaders = [get_nasa_loaders(window_size=config.ws, root_dir="data/nasa", dataset=dataset, filename=f, batch_size=config.bs) for f in file]
+
+    elif dataset == "smd":
+        from dataset.smd import get_loaders as get_smd_loaders, machines
+        loaders = [get_smd_loaders(window_size=config.ws, root_dir="data/smd/processed", machine=m, batch_size=config.bs) for m in machines]
+
+    elif dataset == "swat":
+        from dataset.swat import get_loaders as get_swat_loaders
+        loaders = [get_swat_loaders(window_size=config.ws, root_dir="data/swat", batch_size=config.bs)]
+        
+    return loaders
+
+
+
 def load_results(filename="aucs.json"):
     with open(filename, 'r') as f:
         results = json.load(f)
@@ -12,3 +70,5 @@ def save_results(filename, dataset, model, score):
     results[dataset][model] = score
     with open(filename, "w") as f:
         json.dump(results, f)
+
+
