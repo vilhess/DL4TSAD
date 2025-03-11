b4469d6fa98e85ffb8215b2ada6faa245202cc38
vilhess <samy.vilhes@gmail.com>
2025-03-11 13:52:09 +0100

    fresh

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import numpy as np
 import lightning as L
 import wandb
 from pytorch_lightning.loggers import WandbLogger
+from sklearn.metrics import roc_auc_score
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
 import gc
 
-from eval.get_metric import get_metrics
 from utils import load_model, get_loaders, save_results
 
 torch.multiprocessing.set_sharing_strategy('file_system')
@@ -28,16 +28,12 @@ def main(cfg: DictConfig):
     model_name = cfg.model.name
     dataset = cfg.dataset.name
     config = cfg.dataset_model
-    method = cfg.method.name
-    threshold = cfg.dataset.threshold
     
     model = load_model(model_name)
-
     loaders = get_loaders(dataset, config)
 
     wandb_logger = WandbLogger(project='DL4TSAD', name=f"{model_name}_{dataset}")
-
-    aucs, f1, f1_adjusted = [], [], []
+    aucs = []
     
     for i, (trainloader, testloader) in enumerate(loaders):
         torch.manual_seed(0)
@@ -73,16 +69,11 @@ def main(cfg: DictConfig):
         test_errors = torch.cat(test_errors).detach().cpu()
         test_labels = torch.cat(test_labels).detach().cpu()
 
-        results = get_metrics(test_labels, test_errors, return_f1=False, method=method, threshold=threshold)
-        print(f"Results: {results}")
+        auc = roc_auc_score(test_labels, test_errors)
+        print(f"AUC: {auc}")
 
-        aucs.append(results["auc"])
-        #f1.append(results["f1"])
-        #f1_adjusted.append(results["f1_adjusted"])
-
-        wandb_logger.experiment.results[f"auc_subset_{i+1}/{len(loaders)}"] = results["auc"]
-        #wandb_logger.experiment.config[f"f1_subset_{i+1}/{len(loaders)}_{method}"] = results["f1"]
-        #wandb_logger.experiment.config[f"f1_adjusted_subset_{i+1}/{len(loaders)}_{method}"] = results["f1_adjusted"]
+        aucs.append(auc)
+        wandb_logger.experiment.results[f"auc_subset_{i+1}/{len(loaders)}"] = auc
 
         # To empty the gpu after each loop
         LitModel.to("cpu")
@@ -96,21 +87,11 @@ def main(cfg: DictConfig):
         torch.cuda.empty_cache()
         
     final_auc = np.mean(aucs)
-    #final_f1 = np.mean(f1)
-    #final_adjusted = np.m.detach().cpu()ean(f1_adjusted)
-
     print(f"Final AUC: {final_auc}")
-    #print(f"Final F1: {final_f1}")
-    #print(f"Final F1-Adjusted: {final_adjusted}")
-
     save_results(filename="results/aucs.json", dataset=dataset, model=f'{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}', score=round(final_auc, 4))
-    #save_results(filename="results/f1.json", dataset=dataset, model=f"{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}_{method}", score=round(final_f1, 4))
-    #save_results(filename="results/f1_adjusted.json", dataset=dataset, model=f"{model_name}{"_rev" if hasattr(config, "revin") and config.revin else ""}_{method}", score=round(final_adjusted, 4))
-
     wandb_logger.experiment.results["final_auc"] = final_auc
-    #wandb_logger.experiment.config[f"final_f1_{method}"] = final_f1
-    #wandb_logger.experiment.config[f"final_f1_adjusted_{method}"] = final_adjusted
-    #wandb.finish()
+
+    wandb.finish()
 
 if __name__ == "__main__":
     main()
