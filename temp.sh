efc6818e1c2aebdc86d8e11c62c39ad5042f3681
vilhess <samy.vilhes@gmail.com>
2025-04-30 15:57:00 +0200

    fresh

--- temp.sh
+++ temp.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Lancer les simulations avec GAT
-python multi_simu.py dataset=smd model=gat
-python multi_simu.py dataset=smap model=gat
+python multi_simu.py dataset=smap model=tranad 
+python multi_simu.py dataset=smd model=tranad
 
-# Lancer les simulations avec PatchTST (sans ReVIN)
-python multi_simu.py dataset=swat model=patchtst dataset_model.revin=0
-python multi_simu.py dataset=msl model=patchtst dataset_model.revin=0
-python multi_simu.py dataset=smd model=patchtst dataset_model.revin=0
-python multi_simu.py dataset=smap model=patchtst dataset_model.revin=0
+python multi_simu.py dataset=smap model=lstm 
+python multi_simu.py dataset=smd model=lstm
 
-# Lancer les simulations avec PatchAD
-python multi_simu.py dataset=msl model=patchad
-python multi_simu.py dataset=smd model=patchad
-python multi_simu.py dataset=smap model=patchad
+
