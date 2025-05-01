ea812379f9c8c2d5edd9a35cdfd2c31339fe2155
vilhess <samy.vilhes@gmail.com>
2025-05-01 13:40:58 +0200

    fresh

--- temp.sh
+++ temp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-python multi_simu.py dataset=smap model=tranad 
-python multi_simu.py dataset=smd model=tranad
-
-python multi_simu.py dataset=smap model=lstm 
 python multi_simu.py dataset=smd model=lstm
+python multi_simu.py dataset=smap model=lstm
 
-
+python multi_simu.py dataset=smap model=doc
