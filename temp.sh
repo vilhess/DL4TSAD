805ac56fb495c57f305fd3eb8b07163b981bf2fb
vilhess <samy.vilhes@gmail.com>
2025-05-08 18:59:03 +0200

    anotrans

--- temp.sh
+++ temp.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-python multi_simu.py dataset=smap model=lstm dataset_model.revin=0
-python multi_simu.py dataset=smd model=lstm dataset_model.revin=0
-
-
-python multi_simu.py dataset=smd model=anotrans
 python multi_simu.py dataset=smap model=anotrans
-
+python multi_simu.py dataset=smd model=anotrans
