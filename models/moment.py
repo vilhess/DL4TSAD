2361f856668bbedb65ce70c3afe4ab1acb988a41
vilhess <samy.vilhes@gmail.com>
2025-05-09 15:45:38 +0200

    correctMoment

--- models/moment.py
+++ models/moment.py
@@ -41,7 +41,8 @@ class MomentAD(nn.Module):
         if mode == "train":
 
             batch_x = batch_x.reshape(bs*d, 1, l)
-            batch_masks = batch_masks.repeat_interleave(d, dim=1)
+            batch_masks = batch_masks.repeat_interleave(d, dim=0)
+            print(batch_masks.shape, batch_x.shape)
             masks = self.masking.generate_mask(x=batch_x, input_mask=batch_masks).to(batch_x.device).long()
 
             output = self(x_enc=batch_x, input_mask=batch_masks, mask=masks)
